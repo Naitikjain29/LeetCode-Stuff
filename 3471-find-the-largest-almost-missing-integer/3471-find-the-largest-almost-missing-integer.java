f2cab131e06373a7c705c4963be38600d71e8af6
Naitik Jain <naitikj2912@gmail.com>
2026-08-18 21:01:11 +0530

    Time: 6 ms (37.19%), Space: 46.5 MB (17.54%) - LeetHub

--- 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.java
+++ 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.java
@@ -1,29 +1,53 @@
-import java.util.*;
-
 class Solution {
     public int largestInteger(int[] nums, int k) {
-
-        Map<Integer, Integer> map = new HashMap<>();
         int n = nums.length;
-
-        for (int i = 0; i <= n - k; i++) {
-
-            Set<Integer> set = new HashSet<>();
-
-            for (int j = i; j < i + k; j++) {
-                set.add(nums[j]);
+        
+        // If k == n, whole array is only one subarray
+        if (k == n) {
+            int ans = -1;
+            for (int x : nums) {
+                ans = Math.max(ans, x);
             }
-
-            // Count this window for every distinct number
-            for (int x : set) {
-                map.put(x, map.getOrDefault(x, 0) + 1);
-            }
+            return ans;
         }
 
         int ans = -1;
 
-        for (int x : map.keySet()) {
-            if (map.get(x) == 1) {
+        // First element appears in only first window
+        ans = Math.max(ans, nums[0]);
+
+        // Last element appears in only last window
+        ans = Math.max(ans, nums[n - 1]);
+
+        // But if same element occurs multiple times,
+        // it may appear in more than one window.
+        java.util.HashSet<Integer> set = new java.util.HashSet<>();
+
+        for (int i = 0; i < n - k + 1; i++) {
+            for (int j = i; j < i + k; j++) {
+                set.add(nums[j]);
+            }
+        }
+
+        // Simple correct approach
+        java.util.HashMap<Integer, Integer> count = new java.util.HashMap<>();
+
+        for (int i = 0; i <= n - k; i++) {
+            java.util.HashSet<Integer> window = new java.util.HashSet<>();
+
+            for (int j = i; j < i + k; j++) {
+                window.add(nums[j]);
+            }
+
+            for (int x : window) {
+                count.put(x, count.getOrDefault(x, 0) + 1);
+            }
+        }
+
+        ans = -1;
+
+        for (int x : count.keySet()) {
+            if (count.get(x) == 1) {
                 ans = Math.max(ans, x);
             }
         }
